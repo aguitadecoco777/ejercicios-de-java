b3549ada57c5142907ca62dbc6e3df54dd8a98f7
andres <afguevarac@ufpso.edu.co>
2024-09-30 21:19:23 -0500

    ejercico_dos

---
+++ ejercicio2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre=sc.nextLine();
+        System.out.println("ingrese su edad");
+        int edad=sc.nextInt();
+        System.out.println("ingrese su altura");
+        double altura=sc.nextDouble();
+        System.out.println("hola señor "+nombre+" su edad es de "+edad+" y su estatura es de "+altura);
+    }
+}
