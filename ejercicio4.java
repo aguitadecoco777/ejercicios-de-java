7aa2f911d58783bf046f064bf8baea922a0f8134
andres <afguevarac@ufpso.edu.co>
2024-10-01 19:29:55 -0500

    quinto_ejercicio

--- ejercicio4.java
+++ ejercicio4.java
@@ -4,16 +4,16 @@ public class ejercicio4 {
         Scanner sc=new Scanner(System.in);
         System.out.println("ingrese la cantidad de camisas");
         int cantidadShirt=sc.nextInt();
-        final int PRECIOSHIRT=25;
-        final int PRECIOPANTALON=30;
+        final int PRECIO_SHIRT=25;
+        final int PRECIO_PANTALON=30;
         if (cantidadShirt==1){
             double descuento=0.85;
-            double precioTotal=(double)(descuento)*(PRECIOSHIRT+PRECIOPANTALON);
+            double precioTotal=(double)(descuento)*(PRECIO_SHIRT+PRECIO_PANTALON);
             System.out.println("el precio total es de "+precioTotal);
         }else if(cantidadShirt==2) {
             double descuentoShirt=0.80;
             double descuentoPantalon=0.85;
-            double precioTotal=(double)(descuentoShirt*PRECIOSHIRT)+(descuentoPantalon*PRECIOPANTALON);
+            double precioTotal=(double)(descuentoShirt*PRECIO_SHIRT)+(descuentoPantalon*PRECIO_PANTALON);
             System.out.println("el precio total es de "+precioTotal);
         }
     }
---
+++ ejercicio5.java
@@ -0,0 +1,9 @@
+public class ejercicio5 {
+    public static void main(String[] args) {
+        final int SEMANAS_MES=4;
+        final int DINERO_INICIAL=1000;
+        final int DINERO_RETIRO=200;
+        int dineroFinal=DINERO_INICIAL-(DINERO_RETIRO*SEMANAS_MES);
+        System.out.println("el dinero que queda en la cuenta es el "+dineroFinal);
+    }
+}
